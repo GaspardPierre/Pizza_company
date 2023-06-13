2cdc071f85f2b0201d811b36e3421630385e97ef
GaspardPierre <pierredillard@gmail.com>
2023-06-13 14:22:49 +0200

    add style tailwind part-1

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant.js";
+import MenuItem from "./MenuItem.jsx";
+
+
+
+function Menu() {
+   const menu = useLoaderData();
+   console.log(menu);
+  return <ul>
+    {menu.map((pizza) => <MenuItem pizza={pizza}  key={pizza.id} />)} 
+  </ul>;
+}
+ export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export default Menu;
---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './Header';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loader from './Loader.jsx';
+
+export default function AppLayout() {
+    const navigation = useNavigation ();
+    const isLoading = navigation.state === 'loading';
+
+  return (
+    <div className='layout'>
+    {isLoading && <Loader />}
+        <Header />
+        <main>
+            <h1>
+               
+            </h1>
+            <Outlet />
+        </main>
+        <CartOverview />
+    </div>
+  )
+}
