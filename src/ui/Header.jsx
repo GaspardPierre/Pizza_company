86cf5f90f2c1d66d8f967e9969be65b067a0b5a2
GaspardPierre <pierredillard@gmail.com>
2024-01-30 12:31:58 +0100

    add pizza photo

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,17 +1,17 @@
-
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
 import UserName from '../features/user/UserName';
+import pizza from "/pizza.jpg";
 
 export default function Header() {
   return (
-    <header className=' flex items-center justify-between bg-yellow-400 px-4 py-3 uppercase border-b border-stone-200 sm:px-6 '>
-      <Link to="/"
-      className='tracking-widest'> Pizza Compagny</Link>
+    <header 
+      className='flex items-center justify-between px-4 py-3 uppercase border-b border-stone-200 sm:px-6'
+      style={{ backgroundImage: `url(${pizza})`, backgroundSize: 'cover' }}
+    >
+      <Link to="/" className='tracking-widest text-white'>Pizza Company</Link>
       <SearchOrder />
-   <UserName />
+      <UserName />
     </header>
-  )
+  );
 }
-
-
